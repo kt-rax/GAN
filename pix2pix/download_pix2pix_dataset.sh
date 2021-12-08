955296094af1f8d14ba03ee0d894c42a26afd6e2
KT-Rax <ideallyworld@gmail.com>
2021-12-08 10:22:42 +0800

    Add files via upload

---
+++ pix2pix/download_pix2pix_dataset.sh
@@ -0,0 +1,10 @@
+FILE=$1
+URL=https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/$FILE.tar.gz
+TAR_FILE=./$FILE.tar.gz
+TARGET_DIR=./$FILE/
+wget -N $URL -O $TAR_FILE
+mkdir $TARGET_DIR
+tar -zxvf $TAR_FILE -C ./
+rm $TAR_FILE
+
+https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/facades.tar.gz
