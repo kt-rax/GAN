67e957c6182d393fd6527c7bc5496b1fb248a621
KT-Rax <ideallyworld@gmail.com>
2021-12-15 09:58:24 +0800

    Add files via upload

---
+++ GAN/run.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from train import Trainer
+
+HEIGHT = 28
+WIDTH = 28
+CHANNEL = 1
+LATENT_SPACE_SIZE = 100
+EPOCHS = 5001
+BATCH = 32
+CHECKPOINT = 1000
+MODEL_TYPE = -1
+trainer = Trainer(height=HEIGHT,width = WIDTH,channels=CHANNEL,
+                  latent_size=LATENT_SPACE_SIZE,epochs=EPOCHS,batch=BATCH,checkpoint=CHECKPOINT,model_type=MODEL_TYPE)
+trainer.train()
+
